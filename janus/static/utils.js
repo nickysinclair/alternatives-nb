c726d52872faac167aa49f16339108e9fd36a1a9
Nicky Sinclair <nicky.sinclair@city.ac.uk>
2021-09-21 14:32:03 -0700

    Retrieve selected cell function

---
+++ janus/static/utils.js
@@ -0,0 +1,25 @@
+/*
+ * Set of utility functions
+ */
+
+define(["jquery", "base/js/namespace"], function($, Jupyter) {
+    function retrieveSelectedCells() {
+        /*
+         * Return Array of selected cells and pull out the last one
+         *
+         * Note: returning last cell should be unnecessary as only single cell
+         * can be selected but implementation protects against returning array
+         */
+
+        var nbContainerID = $(Jupyter.notebook.container).prop("id");
+        var selectedCells = $(
+            `#${nbContainerID} > [class~='cell'][class~='selected']`
+        ).get();
+        var lastSelectedCell = selectedCells[selectedCells.length - 1];
+        return lastSelectedCell;
+    }
+
+    return {
+        retrieveSelectedCells: retrieveSelectedCells,
+    };
+});
